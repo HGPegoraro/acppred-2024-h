ef4578b5fa0b0cf70efc7e4c772efb75eace16e5
HGPegoraro <henrique.pegoraro2@gmail.com>
2024-04-10 15:43:50 -0300

    Application created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="acppred",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Henrique Gonçalves Pegoraro",
+    author_email="henrique.pegoraro2@gmail.com",
+    description="Anticancer Peptide Prediction tool"
+)
+
